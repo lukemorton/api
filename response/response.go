36083a3f1a068336e3daae440acb8367bf3f52e1
Luke Morton <lukemorton.dev@gmail.com>
2017-01-15 11:52:22 +0000

    ErrorResponse extends Response

--- response/response.go
+++ response/response.go
@@ -2,13 +2,17 @@ package response
 
 type Response struct {
 	OK bool `json:"ok"`
+}
+
+type ErrorResponse struct {
+	Response
   ErrorMessage string `json:"error,omitempty"`
 }
 
 func OK() Response {
-  return Response{OK: true}
+  return Response{true}
 }
 
-func Error(message string) Response {
-  return Response{OK: false, ErrorMessage: message}
+func Error(message string) ErrorResponse {
+  return ErrorResponse{Response{false}, message}
 }
