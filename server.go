cdbd0347bc9a342e11b3bf591bf95f0b90cddf50
Luke Morton <lukemorton.dev@gmail.com>
2017-01-15 13:40:40 +0000

    Tidy up server code

--- server.go
+++ server.go
@@ -18,21 +18,13 @@ type Server struct {
 	Mux  *http.ServeMux
 }
 
-func (s Server) Handle(path string, h handlerFunc) {
-	s.Mux.Handle(path, handler(func(w http.ResponseWriter, r *http.Request) {
-		response := h(r)
-		jsonResponse(w, response.Status, response.Body)
-	}))
-}
+type HandlerFunc func(r *http.Request) Response
 
-type handlerFunc func(r *http.Request) Response
-
-func handler(h http.HandlerFunc) http.Handler {
-	return handlers.CombinedLoggingHandler(os.Stdout, h)
-}
-
-func jsonResponse(w http.ResponseWriter, status int, body interface{}) {
-	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(body)
+func (s Server) Handle(path string, handler HandlerFunc) {
+	s.Mux.Handle(path, handlers.CombinedLoggingHandler(os.Stdout, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		response := handler(r)
+		w.WriteHeader(response.Status)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response.Body)
+	})))
 }
