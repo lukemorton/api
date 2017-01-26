de6524982e0a406f8d96517f573f1c8c3b3ea205
Luke Morton <lukemorton.dev@gmail.com>
2017-01-26 22:07:42 +0000

    Assert record created in DB

--- users/user_store_test.go
+++ users/user_store_test.go
@@ -12,6 +12,7 @@ func TestCreateUser(t *testing.T) {
 
 	err := users.Create(validUserWithDates())
 	assert.Nil(t, err)
+	assertUserStored(t, users)
 }
 
 func validUserWithDates() User {
@@ -20,4 +21,15 @@ func validUserWithDates() User {
 		UpdatedAt: time.Now(),
 		Email:     "lukemorton.dev@gmail.com",
 	}
+}
+
+func assertUserStored(t *testing.T, db *UserStore) {
+	var email string
+	err := db.Get(&email, "SELECT email FROM users")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, email, "lukemorton.dev@gmail.com")
 }
