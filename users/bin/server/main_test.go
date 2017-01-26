cc6606bb1cfdc58a1a087854fc6ed61d38d5f466
Luke Morton <lukemorton.dev@gmail.com>
2017-01-26 21:39:57 +0000

    Parse response JSON to make test less brittle

--- users/bin/server/main_test.go
+++ users/bin/server/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"net/http"
@@ -15,9 +16,10 @@ func TestStatus(t *testing.T) {
 }
 
 func TestRegister(t *testing.T) {
-	user := []byte(`{"email":"lukemorton.dev@gmail.com"}`)
-	w := POST("/register.json", user)
-	assert.Contains(t, w.Body.String(), `"email":"lukemorton.dev@gmail.com"`, "includes email")
+	w := POST("/register.json", h{"email": "lukemorton.dev@gmail.com"})
+	response := jsonResponse(w)
+	assert.Equal(t, w.Code, 200, "status should be 200")
+	assert.Equal(t, response["email"], "lukemorton.dev@gmail.com", "includes email")
 }
 
 func TestBadRequest(t *testing.T) {
@@ -37,13 +39,22 @@ func GET(path string) *httptest.ResponseRecorder {
 	return request("GET", path, nil)
 }
 
-func POST(path string, body []byte) *httptest.ResponseRecorder {
-	return request("POST", path, bytes.NewBuffer(body))
+func POST(path string, body interface{}) *httptest.ResponseRecorder {
+	jsonBody, _ := json.Marshal(body)
+	return request("POST", path, bytes.NewBuffer(jsonBody))
 }
+
+type h map[string]string
 
 func request(method string, path string, body io.Reader) *httptest.ResponseRecorder {
 	r, _ := http.NewRequest(method, path, body)
 	w := httptest.NewRecorder()
 	AppEngine().ServeHTTP(w, r)
 	return w
+}
+
+func jsonResponse(w *httptest.ResponseRecorder) h {
+	var response *h
+	json.Unmarshal(w.Body.Bytes(), &response)
+	return *response
 }
