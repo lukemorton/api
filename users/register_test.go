d9bd24191e162250d2b82d45038ef1900c14169e
Luke Morton <lukemorton.dev@gmail.com>
2017-01-28 20:39:59 +0000

    Add more tests around registration

--- users/register_test.go
+++ users/register_test.go
@@ -1,12 +1,17 @@
 package users
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestRegisterValidUser(t *testing.T) {
-	user, err := Register(mockUserCreator{}, validUser())
+	user, err := Register(mockUserCreator{}, RegisterUser{
+		Email:    "lukemorton.dev@gmail.com",
+		Password: "bob",
+	})
+
 	assert.Nil(t, err)
 	assert.NotNil(t, user.Id)
 	assert.NotNil(t, user.CreatedAt)
@@ -14,15 +19,37 @@ func TestRegisterValidUser(t *testing.T) {
 	assert.NotNil(t, user.PasswordHash)
 }
 
-func validUser() RegisterUser {
-	return RegisterUser{
-		Email:    "lukemorton.dev@gmail.com",
+func TestRegisterUserWithoutEmail(t *testing.T) {
+	_, err := Register(mockUserCreator{}, RegisterUser{
 		Password: "bob",
-	}
+	})
+
+	assert.EqualError(t, err, "User requires email address")
 }
 
-type mockUserCreator struct{}
+func TestRegisterUserWithoutPassword(t *testing.T) {
+	_, err := Register(mockUserCreator{}, RegisterUser{
+		Email: "lukemorton.dev@gmail.com",
+	})
+
+	assert.EqualError(t, err, "User requires password")
+}
+
+func TestUserCreatorError(t *testing.T) {
+	users := mockUserCreator{errors.New("Uh oh")}
+
+	_, err := Register(users, RegisterUser{
+		Email:    "lukemorton.dev@gmail.com",
+		Password: "bob",
+	})
+
+	assert.EqualError(t, err, "Uh oh")
+}
+
+type mockUserCreator struct {
+	err error
+}
 
 func (users mockUserCreator) Create(user *User) error {
-	return nil
+	return users.err
 }
