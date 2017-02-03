d80d131d652727dbaad7ab3ebfb0b19989672c29
Luke Morton <lukemorton.dev@gmail.com>
2017-02-03 09:06:41 +0000

    Whitespace

--- front/bin/server/main.go
+++ front/bin/server/main.go
@@ -2,40 +2,40 @@ package main
 
 import (
 	"gopkg.in/gin-gonic/gin.v1"
-  "log"
-  "net/http"
-  "net/http/httputil"
-  "net/url"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
 func main() {
-  hs := hostSwitch{}
+	hs := hostSwitch{}
 
-  hs["default"] = defaultRouter()
-  hs["users.localhost:3000"] = wrapProxy(users())
-  hs["authors.localhost:3000"] = wrapProxy(authors())
+	hs["default"] = defaultRouter()
+	hs["users.localhost:3000"] = wrapProxy(users())
+	hs["authors.localhost:3000"] = wrapProxy(authors())
 
-  log.Fatal(http.ListenAndServe("0.0.0.0:3000", hs))
+	log.Fatal(http.ListenAndServe("0.0.0.0:3000", hs))
 }
 
 type hostSwitch map[string]http.Handler
 
 func (hs hostSwitch) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  log.Println(r.Host)
-  if handler := hs[r.Host]; handler != nil {
-    handler.ServeHTTP(w, r)
-  } else {
-    hs["default"].ServeHTTP(w, r)
-  }
+	log.Println(r.Host)
+	if handler := hs[r.Host]; handler != nil {
+		handler.ServeHTTP(w, r)
+	} else {
+		hs["default"].ServeHTTP(w, r)
+	}
 }
 
 func wrapProxy(proxy http.Handler) *gin.Engine {
 	http := gin.Default()
 
-  http.Use(func(c *gin.Context) {
-    proxy.ServeHTTP(c.Writer, c.Request)
+	http.Use(func(c *gin.Context) {
+		proxy.ServeHTTP(c.Writer, c.Request)
 		c.Abort()
-  })
+	})
 
 	return http
 }
@@ -51,11 +51,11 @@ func defaultRouter() *gin.Engine {
 }
 
 func users() *httputil.ReverseProxy {
-  url, _ := url.Parse("http://localhost:3001")
-  return httputil.NewSingleHostReverseProxy(url)
+	url, _ := url.Parse("http://localhost:3001")
+	return httputil.NewSingleHostReverseProxy(url)
 }
 
 func authors() *httputil.ReverseProxy {
-  url, _ := url.Parse("http://localhost:3002")
-  return httputil.NewSingleHostReverseProxy(url)
+	url, _ := url.Parse("http://localhost:3002")
+	return httputil.NewSingleHostReverseProxy(url)
 }
